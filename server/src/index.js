4a72b270024a4374ee5ae6bc873e4af20f9096d3
Technical73 <w50790679@gmail.com>
2025-05-30 22:55:33 +0530

    completed

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://your-netlify-site.netlify.app",
+    origin: "https://imdbd.netlify.app",
     credentials: true,
   })
 );
